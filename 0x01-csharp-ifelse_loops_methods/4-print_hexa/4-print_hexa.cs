66d4cd53806d606425474d3a034dc8398efa512f
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 16:20:00 -0500

    Convert decimal to hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int dec = 0; dec <= 98; dec++)
+            Console.WriteLine("{0:D} = 0x{0:x}", dec);
+        }
+    }
+}
