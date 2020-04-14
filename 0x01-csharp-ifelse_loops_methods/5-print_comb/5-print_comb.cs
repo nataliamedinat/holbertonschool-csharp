4419c96b0dc487788816f7660d1b6ba7f98e1171
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 17:09:37 -0500

    kind of..

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (number = 0; number <= 99; number++)
+            Console.Write(@"{0}{1:D2}, ", number);
+                  
+        }
+    }
+}
