db4f7e690ed6bbab721f7271052b6c97ff28ee81
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 19:19:00 -0500

    All possible combinations of 2 digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2 ;
+            for (num1 = 0; num1 <= 8; num1++)
+            {
+                for (num2 = 1; num2 <= 9; num2++)
+                {
+                    if (num1 == 8 && num2 == 9)
+                        Console.Write("{0}{1}\n", num1, num2);
+                    else if (num1 < num2)
+                        Console.Write(@"{0}{1}, ", num1, num2);
+                }
+            }
+        }
+    }
+}
