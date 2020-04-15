83f3e18ef19d3e422286ffcd68c676fd9060d96f
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 22:36:20 -0500

    Fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                    Console.Write("FizzBuzz ");
+                else if (num % 3 == 0)
+                    Console.Write("Fizz ");
+                else if  (num % 5 == 0)
+                    Console.Write("Buzz ");
+                else
+                    Console.Write("{0} ", num);
+            }
+        }
+    }
+}
