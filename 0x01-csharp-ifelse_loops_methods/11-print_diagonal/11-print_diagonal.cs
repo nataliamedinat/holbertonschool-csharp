e8e810297e5d9ae1617c8c7dedc44069b2ce9a55
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 21:57:57 -0500

    Diagonal line

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int trav1 = 0;
+        for (; trav1 < length; trav1++)
+        {
+            int trav2 = 0;
+            for (; trav2 < trav1; trav2++)
+                Console.Write(" ");
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine("");
+    }
+}
