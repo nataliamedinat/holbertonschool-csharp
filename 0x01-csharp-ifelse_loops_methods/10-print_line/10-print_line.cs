615dcb12a90d1697b4d607ecdf7ced86c93c44fb
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-14 20:54:36 -0500

    Prin a _ in base of a length

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            int trav ;
+            
+                if (length <= 0 )
+                    Console.Write("\n");
+                for (trav = 1; trav <= length; trav++)
+                {
+                    Console.Write(@"_");
+                }
+                
+        }
+    }
