1918dcbefbf328ae627fb307811a82889b310e18
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 18:30:52 -0500

    Print a list of int in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    class Array
+    {
+        public static void Reverse(int[] array)
+        {
+            if (array == null)
+            {
+                Console.WriteLine("\n");
+                return ;
+            }
+
+            for (int value = array.Length -1; value >= 0; value--)
+            {  
+                if (value == 0)
+                {
+                    Console.WriteLine(array[value]);
+                    break;
+                }
+                 Console.Write (array[value] + " ");
+            }
+        }
+    }
