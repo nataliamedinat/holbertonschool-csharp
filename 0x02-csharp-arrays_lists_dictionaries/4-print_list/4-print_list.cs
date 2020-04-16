c78b1b838ad5292748103030d140d5350c7cc87d
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 18:59:42 -0500

    Create and prints a list of int

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+
+            List <int> newList = new List <int> ();
+            if (size == 0)
+            {
+                Console.Write("\n");
+                return newList;
+            }
+
+            for (int trav = 0; trav < size; trav++)
+            {
+                newList.Add(trav);
+                if (trav == size - 1)
+                {
+                    Console.WriteLine(trav);
+                    break;
+                }
+                Console.Write (trav + " ");
+            }
+            return newList;
+        }
+    }
