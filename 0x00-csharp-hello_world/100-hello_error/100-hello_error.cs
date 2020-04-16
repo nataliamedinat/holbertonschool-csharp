47ca429d910fba565ed48f1ea370a835b5f8953e
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-15 19:21:06 -0500

    Change the environment exit and print wit stderr

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.TextWriter errorWriter = Console.Error;
+        errorWriter.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+            Environment.Exit(1);
+        }
+    }
+}
