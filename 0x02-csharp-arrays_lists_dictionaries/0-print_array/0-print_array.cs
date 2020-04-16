3349ea5b72f963bbbb65690f71c13a37041cc6f2
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 17:05:47 -0500

    Add a condition when size is equal to 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,13 +4,20 @@
     {
         public static int[] CreatePrint(int size)
         {
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            int [] newarr = new int [size];
-            for (int trav=0; trav < newarr.Length; trav++)
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int [] newarr = new int[size];
+        if (size == 0)
+        {
+            Console.Write("\n");
+            return newarr;
+        }
+
+        for (int trav=0; trav < newarr.Length; trav++)
             {
                 Console.Write (trav + " ");
             }
