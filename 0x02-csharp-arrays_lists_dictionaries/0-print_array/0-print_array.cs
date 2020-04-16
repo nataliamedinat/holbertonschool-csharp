be3fd072374f9873495383c84cabac3a45840a76
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 16:38:32 -0500

    Create an array of a given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int [] newarr = new int [size];
+            for (int trav=0; trav < newarr.Length; trav++)
+            {
+                Console.Write (trav + " ");
+            }
+            Console.WriteLine("");
+            return newarr;
+        }
+    }
