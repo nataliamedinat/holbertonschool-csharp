97ee83a417eda75d54567486eb744b5f886d13fe
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 23:35:29 -0500

    Return the number of keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int counter = 0;
+            foreach (var key in myDict)
+            {
+                counter++;
+            }
+            return (counter);
+        }
+    }
+
