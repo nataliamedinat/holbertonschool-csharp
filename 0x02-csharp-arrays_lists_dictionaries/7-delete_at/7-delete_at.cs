32ce170f549e21c270f798349fb497267d3a3828
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 20:26:39 -0500

    Delete an item

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DeleteAt(List<int> myList, int index)
+        {
+             if (index >= 0 && index < myList.Count)
+             {
+                 myList.Remove(myList[index]);
+                 return myList;
+             }
+             else
+             {
+                 Console.WriteLine("Index is out of range");
+                 return myList;
+             }
+        }
+    }
