89fbecd37bbacb73e4dbe3c43e9a85e41bc791f3
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-17 16:30:45 -0500

    Multidimensional array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 5;
+            int height = 5;
+            int [,] multi = new int [width, height];
+            multi [2, 2] = 1;
+
+            for (int wth = 0; wth < width; wth++)
+            {
+                for (int hth= 0; hth < height; hth++)
+                {
+                    if (hth < height - 1)
+                    {
+                        Console.Write(multi[wth, hth] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(multi[wth, hth]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
