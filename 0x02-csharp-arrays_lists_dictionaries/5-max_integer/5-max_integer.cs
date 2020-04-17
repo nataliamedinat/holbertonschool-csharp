eb64cf3854933d65473f4c663bdc47c86c9765d5
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-16 19:40:03 -0500

    Bigger int in a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if (myList == null || myList.Count == 0)
+            {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+            int bigInt = myList.Count - 1;
+            myList.Sort();
+            return myList[bigInt];
+        }
+    }
