ff0e954d9671fefdcbc68295f03f0349f502a79b
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-17 15:37:51 -0500

    Sort a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            var lists = myDict.Keys.ToList();
+            lists.Sort();
+
+        foreach (var key in lists)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }   
+        }
+    }
