637e9d7797939d15ebb0405756609034360efbab
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-17 17:00:16 -0500

    Divisible by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        int value;
+        List<bool> newList = new List<bool> ();
+        for (value = 0; value < myList.Count; value++) 
+        {
+            if (myList[value] % 2 == 0) {
+            newList.Add(true);
+        }
+            else {
+                newList.Add(false);
+            }
+        } 
+        return newList;
+    }
+}
