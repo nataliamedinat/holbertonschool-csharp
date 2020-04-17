0df7480b16df1a30f862d19287d9d1c08bdc4e42
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-17 01:44:27 -0500

    All values * 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+        {
+            Dictionary<string, int> Ndict = new Dictionary<string, int> ();
+            foreach (string item in myDict.Keys)
+            {
+                Ndict.Add(item, myDict[item] * 2);
+            }
+            return Ndict;
+        }
+    }
+
