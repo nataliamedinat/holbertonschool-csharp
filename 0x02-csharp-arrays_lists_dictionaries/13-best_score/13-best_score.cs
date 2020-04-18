69cd8d5b429815695df0b9466beece70db4fd416
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-17 19:32:03 -0500

    higher valie

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = 0;
+        string name = "None";
+        foreach (KeyValuePair<string, int> val in myList)
+        {
+            if (val.Value > max)
+            {
+                max = val.Value;
+                name = val.Key;
+            }
+        }
+        return name;
+    }
+}
