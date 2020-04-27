cec16775ee8166b351d60230f06cc55610dd8215
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 18:15:47 -0500

    Common items on a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+  public static List<int> CommonElements(List<int> list1, List<int> list2)
+  {
+    List<int> common = new List<int>();
+    foreach(int elem in list1)
+    {
+      if (list2.Contains(elem))
+        common.Add(elem);
+    }
+    common.Sort();
+    return common;
+  }
+}
