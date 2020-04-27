6d506c3bbff7a7cae12c63611c96c94ac878e7ed
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 03:53:20 -0500

    Add all unique numbers from a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int unique = 0;
+            HashSet<int> set = new HashSet<int>(myList);
+            foreach (int number in set)
+            {
+                unique += number;
+            }
+            return unique;
+            
+        }
+    }
