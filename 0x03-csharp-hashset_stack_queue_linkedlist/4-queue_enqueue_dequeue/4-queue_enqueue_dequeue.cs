ccc6dbbf4fc87e1314f86708cd0075179fb1fca7
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 18:22:56 -0500

    Queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class MyQueue
+    {
+        public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+        {
+            // Total number of items
+            Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+            if (aQueue.Count <= 0)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("First item: {0}", aQueue.Peek());
+            }
+
+            // search for an item
+            bool elem = true;
+            elem = (aQueue.Contains(search));
+            Console.WriteLine($"Queue contains \"{search}\": {elem}");
+
+            if (elem)
+            {
+                while (aQueue.Dequeue() != search);
+            }
+            aQueue.Enqueue(newItem); 
+            return aQueue;
+    }
+        }
