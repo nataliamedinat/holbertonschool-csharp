8bcd61e0f33e639057c81422918d69096dde2208
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 18:56:42 -0500

    Get the lenght of a linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+    class LList
+    {
+        public static int Length(LinkedList<int> myLList)
+        {
+            int num = 0;
+            foreach (int elem in myLList)
+            {
+                num ++;
+            }
+            return num;
+        }
+    }
+
