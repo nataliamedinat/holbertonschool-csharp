33539cf561e791c9144e1705ab230c6c2ba72da9
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 05:42:36 -0500

    Stack methods

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class MyStack
+    {
+        public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+        {
+            // Total number of items
+            Console.WriteLine("Number of items: {0}", aStack.Count);
+
+            if (aStack.Count <= 0)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("Top item: {0}", aStack.Peek());
+            }
+
+            // search for an item
+            bool elem = true;
+            Console.WriteLine("Stack contains {0}: {1}", search, aStack.Contains(search));
+
+            if (elem)
+            {
+                while (aStack.Pop() != search);
+            }
+            aStack.Push(newItem); 
+            return aStack;
+    }
+    }
