6b9f69ae69b1879bd88c709eb7c23c565b1696a3
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 18:39:48 -0500

    Create and print a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static LinkedList<int> CreatePrint(int size)
+        {
+            LinkedList<int> newlist = new LinkedList<int> ();
+            if (size < 0)
+            {
+                return newlist;
+            }
+
+            for (int trav = 0; trav < size; trav++)
+            {
+                newlist.AddLast(trav);
+                Console.WriteLine(trav);
+            }
+            return newlist;
+
+        }
+    }
+
