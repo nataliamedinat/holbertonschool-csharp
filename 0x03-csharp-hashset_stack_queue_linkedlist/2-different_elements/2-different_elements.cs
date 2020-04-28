92b53636340fcae2c7df3e7cf869e1d4da8ec808
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 23:25:30 -0500

    not in both lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            List<int> DList = new List<int>();
+
+            for (int elem = 0; elem < list1.Count ; elem++)
+            {
+                if (list2.Contains(list1[elem]))
+                {
+                    continue;
+                }
+                DList.Add(list1[elem]);
+            }
+
+            for (int el = 0; el < list2.Count; el++)
+            {
+                if (list1.Contains(list2[el]))
+                {
+                    continue;
+                }
+                DList.Add(list2[el]);
+            }
+
+            DList.Sort();
+            return DList;
+        }
+    }
