7cce2acf57476a0362169deb3cc20e69c00865ad
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 20:11:59 -0500

    Remove the head node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int Pop(LinkedList<int> myLList)
+        {
+            int headnode = myLList.First.Value;
+            if (myLList.Count <= 0)
+            {
+                return 0;
+            }
+            myLList.RemoveFirst();
+            return headnode;
+        }
+    }
