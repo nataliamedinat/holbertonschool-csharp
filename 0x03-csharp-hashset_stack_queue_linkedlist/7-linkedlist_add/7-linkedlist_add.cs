610e365f8c4f1dab40966c970e57643d27f6a488
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 19:07:42 -0500

    Add a node at the beggining

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+        {
+            return myLList.AddFirst(n);
+        }
+    }
+
