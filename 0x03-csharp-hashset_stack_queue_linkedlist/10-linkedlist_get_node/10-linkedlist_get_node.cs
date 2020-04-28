0b2ee555926f8afecefbe5a36e75da8e024edcdd
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-27 21:34:40 -0500

    Get the value of the node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int GetNode(LinkedList<int> myLList, int n)
+        {
+            int index = 0;
+            foreach (int elem in myLList)
+            {
+                if (index == n)
+                {
+                    return (elem);
+                }
+                index++;
+            }
+            return 0;
+        }
+    }
