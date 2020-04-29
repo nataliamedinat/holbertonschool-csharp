476c1a3e663d092e39079ef156958f5ba4cdfaec
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-28 19:46:15 -0500

    Constructor that takes parameters

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum Rating 
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+ public Dog(string DogName, float DogAge, string DogOwner, Rating DogRating)
+ {
+    name = DogName;
+    age = DogAge;
+    owner = DogOwner;
+    rating = DogRating;
+ }
+}
