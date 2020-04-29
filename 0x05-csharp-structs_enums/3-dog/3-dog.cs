23c1153b9112e34d56a6f407d9a0cf736ce04b79
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-28 20:11:32 -0500

    Override tostring method

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum Rating 
+{
+    Good,
+    Great,
+    Excellent
+}
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+ public Dog(string DogName, float DogAge, string DogOwner, Rating DogRating)
+ {
+    name = DogName;
+    age = DogAge;
+    owner = DogOwner;
+    rating = DogRating;
+ }
+
+
+ public override string ToString()
+ {
+     return string.Format($"Dog name: {name}\nAge: {age}\nOwner: {owner}\nRating: {rating}");
+ }
+}
