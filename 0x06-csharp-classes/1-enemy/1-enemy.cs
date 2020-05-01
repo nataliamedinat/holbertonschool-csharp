0cf5c94676bf0d4ab23cb7c537fd1d15567e7fbd
nataliamedinat <nataliamedinabotero@gmail.com>
2020-04-30 19:39:28 -0500

    Added a public field and a constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Represents and define a zombie empty class. </summary>
+    public class Zombie 
+    {
+        /// <summary> Represents the healt of the zombie. </summary>
+        public int health;
+
+        /// <summary> Initializes health in 0. </summary>
+        public Zombie()
+        {
+            health= 0;
+        }
+    }
+}
