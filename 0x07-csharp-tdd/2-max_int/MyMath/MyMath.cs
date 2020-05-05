a8db9cc2381a743cf21eb554f978ba4f89369ff5
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-05 18:14:43 -0500

    Unit testing: Max int in a list

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MyMath
+{
+    /// <summary> Represents class of operations </summary>
+    public class Operations
+    {
+        /// <summary> Return the max int in a list </summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums == null || nums.Count == 0)
+            {
+                return 0;
+            }
+            int max = nums.Max();
+            return (max);
+        }
+    }
+}
