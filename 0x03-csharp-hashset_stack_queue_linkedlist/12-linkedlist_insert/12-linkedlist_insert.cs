fe9b90128aef68c6059d1733448416aba81efa0f
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-05 18:10:02 -0500

    Changes

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+    class LList
+    {
+        public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
