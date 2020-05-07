6a4fcddf7a123f33b6205c8cb6f8d3e24cc39e15
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 17:42:45 -0500

    instance of

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    /// <summary> Object class </summary>
+    class Obj
+    {
+        /// <summary> Check if an obj is an instance </summary>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            return obj is Array;
+        }
+    }
