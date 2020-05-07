0050faaffb33629860d6386f0a9e04d0ee702ba9
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 16:47:07 -0500

    First class

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Obj
+    {
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj is int)
+                return true;
+            else
+                return false;
+        }
+    }
+
