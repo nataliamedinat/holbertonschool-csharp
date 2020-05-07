1d58bee2652fe126c9a68b62cf520312718bf988
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 16:50:07 -0500

    xml documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+    /// <summary> Class object </summary>
     class Obj
     {
+        /// <summary>  Method that returns true if an obj is int, otherwise false</summary>
         public static bool IsOfTypeInt(object obj)
         {
             if (obj is int)
