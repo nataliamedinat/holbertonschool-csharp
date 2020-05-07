45a3ab7654fe3e21c8e40715234e0caf402acda2
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 18:05:02 -0500

    Is a subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    /// <summary> Object class</summary>
+    class Obj
+    {
+        /// <summary> Check if an obj is an instance of a class</summary>
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return derivedType.IsSubclassOf(baseType);
+        }
+    }
