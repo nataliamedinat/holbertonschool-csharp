6ca7ec032cdce0b954fc181322055a1d5fb27572
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 19:02:48 -0500

    Define two classes

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    /// <summary> Class animal </summary>
+    public class Animal
+    {
+    }
+
+    /// <summary> Class that inherites from animal</summary>
+    public class Dog : Animal
+    {
+    }
