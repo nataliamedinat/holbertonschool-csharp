82022f2a6a13fe30ff9637650386528808247777
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 23:38:22 -0500

    List method and properties

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+    /// <summary> Object class </summary>
+    class Obj
+    {
+        /// <summary> Prints the names of the available properties and methods of an object.</summary>
+        public static void Print(object myObj)
+        {
+            Type o = myObj.GetType();
+            MethodInfo[] methods = o.GetMethods();
+            PropertyInfo[] properties = o.GetProperties();
+            Console.WriteLine($"{o.Name} Properties:");
+            foreach (var p in o.GetProperties())
+            {
+                Console.WriteLine($"{p.Name}");
+            }
+            Console.WriteLine($"{o.Name} Methods:");
+             foreach(MethodInfo method in methods) 
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
