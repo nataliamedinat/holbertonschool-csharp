e14190ff2596c29c98ae70da2711e7073382d54b
natimedinat <nataliamedinabotero@gmail.com>
2020-05-07 19:18:22 -0500

    Class shape w and exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> Shape class </summary>
+    public class Shape
+    {
+        /// <summary> Represents the area of the shape</summary>
+        public virtual int Area()
+        {
+            throw new NotImplementedException ("Area() is not implemented");
+        }
+    }
