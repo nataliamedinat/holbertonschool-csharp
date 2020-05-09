5a328f5051244ba2648e50b2ae7e85be910a41ce
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-08 22:46:48 -0500

    Camel case,e return number of word

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Empty()
+        {
+            {
+            string str = "";
+            var result = Str.CamelCase(str);
+            Assert.AreEqual(0, result);
+        }
+        }
+
+        [Test]
+        public void ReturnNumber()
+        {
+            string str = "lifeIsBeatiful";
+            var result = Str.CamelCase(str);
+            Assert.AreEqual(3, result);
+
+        }
+    }
+}
