6ff402f36081bae4a55c12433a25d5cef0bffe64
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-27 17:56:20 -0500

    typeof

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    ///<summary>Class queue</summary>
+    class Queue<T>
+    {
+        ///<summary>Check an return T type</summary>
+        public  Type CheckType()
+        {
+            return typeof(T);
+        }
+    }
