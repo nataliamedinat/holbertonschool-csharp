8036598e35e9030ee515c07dfd7d4e10095d882c
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-28 18:46:13 -0500

    New declarations

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+    ///<summary>Base class </summary>
+    public abstract class Base
+    {
+        ///<summary>Public property </summary>
+        public string name;
+
+
+        ///<summary>Override tostring method</summary>
+        public override string ToString()
+        {
+              return String.Format ($"{name} is a {this.GetType()}");
+        }      
+    }
+
+    ///<summary>Interface interactive  </summary>
+    public interface IInteractive
+    {
+        ///<summary> Method interact </summary>
+        void Interact();
+    }
+
+    ///<summary> Interface breakable </summary>
+    public interface IBreakable
+    {
+        ///<summary> Property int durability</summary>
+        int durability {get; set;}
+
+        ///<summary> Break method </summary>
+        void Break();
+    }
+
+    ///<summary> Collectable interface </summary>
+    public interface ICollectable
+    {
+        ///<summary> Bool property </summary>
+        bool isCollected {get; set;}
+
+        ///<summary> Collect method </summary>
+        void Collect();
+    }
+
+    ///<summary> Class that inherits from base and from all the interfaces </summary>
+    public class TestObject : Base, IInteractive, ICollectable, IBreakable
+    {
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+
+    }
+
+    public void Break()
+    {
+
+    }
+
+    public void Collect()
+    {
+
+    }
+
+}
