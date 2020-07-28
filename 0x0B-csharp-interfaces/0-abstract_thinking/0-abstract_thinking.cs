3fc06ffecad624ecc5829cd13dd57076e554d545
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-28 17:26:09 -0500

    Override method

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    ///<summary>Base class </summary>
+    public abstract class Base
+    {
+        ///<summary>Public property </summary>
+        public string name;
+
+        ///<summary>Override tostring method</summary>
+        public override string ToString()
+        {
+              return String.Format ($"{name} is a {this.GetType()}");
+        }
+
+        
+    }
