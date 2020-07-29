bd9b2e25878706aca6bc917db17f824f2e3bd37b
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-29 18:14:29 -0500

    New class created

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+///<summary> Class player </summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary> Constructor </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary> Method that prints health </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
