777e534c7a0a99bad419f056fcf6abf2d5c1070d
nataliamedinat <nataliamedinabotero@gmail.com>
2020-07-28 19:30:03 -0500

    new class

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+    ///<summary>Base class </summary>
+    public abstract class Base
+    {
+        ///<summary>Public property </summary>
+        public string name;
+
+        ///<summary>Override tostring method</summary>
+        public override string ToString()
+        {
+              return String.Format ($"{name} is a {this.GetType()}");
+        }      
+    }
+
+    ///<summary>Interface interactive  </summary>
+    interface IInteractive
+    {
+        ///<summary> Method interact </summary>
+        void Interact();
+    }
+
+    ///<summary> Interface breakable </summary>
+    interface IBreakable
+    {
+        ///<summary> Property int durability</summary>
+        int durability {get; set;}
+
+        ///<summary> Break method </summary>
+        void Break();
+    }
+
+    ///<summary> Collectable interface </summary>
+    interface ICollectable
+    {
+        ///<summary> Bool property </summary>
+        bool isCollected {get; set;}
+
+        ///<summary> Collect method </summary>
+        void Collect();
+    }
+
+    class Door : Base, IInteractive
+    {
+        public Door(string named = "Door")
+        {
+            name = named;
+        }
+
+        public void Interact()
+        {
+            Console.WriteLine($"You try to open the {name}. It's locked.");
+        }
+    }
